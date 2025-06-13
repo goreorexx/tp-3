e73c20b3122c7fcfc81696c4c5a375a7f361e5b5
FabianaR <rossifabiana930@gmail.com>
2025-06-13 13:58:59 -0300

    Exportaciones por defecto y nombradas cjs.

---
+++ modulos/locales/cjs/app.cjs
@@ -0,0 +1,5 @@
+const funciones = require("./funcionespordefecto.cjs");
+const {resta,suma} = require("./funcionesnombradas.cjs");
+
+console.log(funciones.suma(5,3));
+console.log(resta(3,2));
---
+++ modulos/locales/cjs/funcionespordefecto.cjs
@@ -0,0 +1,11 @@
+let funciones = {};
+
+funciones.multiplicacion = (num1, num2) => {
+    return num1*num2;
+};
+
+funciones.suma = (num1, num2) => {
+    return num1+num2;
+};
+
+module.exports = funciones;
